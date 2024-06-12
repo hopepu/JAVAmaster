56b77295d9c740297504731e87c2a8979f6b7e5d
hopepu <whdydwo2@gmail.com>
2024-06-12 17:01:37 +0900

    1

---
+++ JAVA master/src/ch03/OperatorEx5.java
@@ -0,0 +1,19 @@
+package ch03;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 4;
+		
+		System.out.println(a + "+" + b + "=" + (a+b));
+		System.out.println(a + "-" + b + "=" + (a-b));
+		System.out.println(a + "X" + b + "=" + a*b);
+		System.out.println(a + "/" + b + "=" + a/b);
+		System.out.printf("%d / %f = %f%n", a, (float)b, a/(float)b);
+		
+		
+	}
+
+}
